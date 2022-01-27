2c22086d950826a539b5717e0a010f9f66e5d9ea
Manoel Rogildo <manoelrogildo@gmail.com>
2022-01-26 21:31:50 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/manoelrogildo/cursomc/dominio/Cliente.java
+++ src/main/java/com/manoelrogildo/cursomc/dominio/Cliente.java
@@ -13,14 +13,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-
+import javax.persistence.OneToMany;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.manoelrogildo.cursomc.dominio.enums.TipoCliente;
 
 @Entity
 public class Cliente implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
@@ -28,16 +28,20 @@ public class Cliente implements Serializable {
 	private String email;
 	private String cpfOuCnpj;
 	private Integer tipo;
-	
+
 	@JsonManagedReference
 	@ManyToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
-	
+
 	@ElementCollection
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
-	
-	public Cliente() {}
+
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+
+	public Cliente() {
+	}
 
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
 		super();
@@ -104,6 +108,15 @@ public class Cliente implements Serializable {
 		this.telefones = telefones;
 	}
 
+	@OneToMany(mappedBy = "cliente")
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -128,8 +141,5 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
-	
-	
+
 }
---
+++ src/main/java/com/manoelrogildo/cursomc/dominio/PagamentoComCartao.java
@@ -0,0 +1,31 @@
+package com.manoelrogildo.cursomc.dominio;
+
+import javax.persistence.Entity;
+
+import com.manoelrogildo.cursomc.dominio.enums.EstadoPagamento;
+
+@Entity
+public class PagamentoComCartao extends Pagamento {
+	private static final long serialVersionUID = 1L;
+	
+	
+	private Integer numeroDeParcelas;	
+
+	public PagamentoComCartao() {}
+
+	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroDeParcelas) {
+		super(id, estado, pedido);
+		this.setNumeroDeParcelas(numeroDeParcelas);
+	}
+
+	public Integer getNumeroDeParcelas() {
+		return numeroDeParcelas;
+	}
+
+	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+	
+	
+	
+}
